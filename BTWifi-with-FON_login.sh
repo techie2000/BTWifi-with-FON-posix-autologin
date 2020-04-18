b629c8e151be8f6b82ce18153ec6c4c0e165db98
techie2000 <38585780+techie2000@users.noreply.github.com>
2020-04-18 17:19:03 +0100

    Update BTWifi-with-FON_login.sh

--- BTWifi-with-FON_login.sh
+++ BTWifi-with-FON_login.sh
@@ -5,8 +5,8 @@ RELOG_UNAME="<username>"
 RELOG_PASSW="<pass>"
 
 #FUNCTION TO CONNECT
-connect_to_wifi () {
-  IS_LOGGED_IN=$(wget "https://www.btwifi.com:8443/home" --no-check-certificate --timeout 15 -O - 2>/dev/null | grep "now logged in")
+am_i_connected_to_wifi () {
+  IS_LOGGED_IN=$(wget "https://www.btopenzone.com:8443/home" --no-check-certificate --timeout 15 -O - 2>/dev/null | grep "now logged in")
 
   if [ "$IS_LOGGED_IN" ]
 
@@ -16,9 +16,15 @@ connect_to_wifi () {
 
   else
     $DBG && echo "You're not logged in... will log in now!"
+    connect_to_wifi
+  fi
+}
+
+connect_to_wifi () {
     $DBG && logger -t "logon_fon" "You're not logged in... will log in now!"
-    OUT=$(wget -qO - --no-check-certificate --post-data "provider=tbb& username=$RELOG_UNAME&password=$RELOG_PASSW" "https://www.btwifi.com:8443/home")
-    ONLINE=$(echo "$OUT" | grep "now logged on" )
+    #OUT=$(wget -O - --no-check-certificate --post-data "provider=btoz&username=$RELOG_UNAME&password=$RELOG_PASSW" "https://www.btopenzone.com:8443/tbbLogon")
+    curl 'https://www.btwifi.com:8443/tbbLogon' -d "username=$RELOG_UNAME&password=$RELOG_PASSW"
+    ONLINE=$(wget "https://www.btopenzone.com:8443/home" --no-check-certificate --timeout 15 -O - 2>/dev/null | grep "now logged in")
     if [ "$ONLINE" ]
     then
       $DBG && echo "You're online!"
@@ -27,7 +33,6 @@ connect_to_wifi () {
       $DBG && echo "Could not login :("
       $DBG && logger -t "logon_fon" "Could not login :("
     fi
-  fi
 }
 
 #GET CURRENT WIFI
@@ -42,8 +47,9 @@ supportedssid="$supportedssid GL-AR750S-730"
 #IF NETWORK IS SUPPROTED, ATTEMPT TO CONNECT
 if [ "${supportedssid##*$myssid*}" ]
 then
-  echo "Connected to a BT wifi net: will try to log in"
-  connect_to_wifi
+  echo "Connected to a BT wifi net. Will check to see if I'm logged in"
+  am_i_connected_to_wifi
 else
   echo "Wifi is $myssid"
+  echo "Will NOT attempt to connect"
 fi
